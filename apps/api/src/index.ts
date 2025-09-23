3981afc45572500c429828266f00b87b91d54634
HayatoKudo <kudoh115@gmail.com>
2025-09-23 16:56:37 +0900

    feat: connect db

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,9 +1,30 @@
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
+import { D1Database } from '@cloudflare/workers-types'
 
-const app = new Hono()
+type Bindings = {
+  keiba_db: D1Database
+}
+
+const app = new Hono<{ Bindings: Bindings }>()
+
+app.use('/*', cors({
+  origin: ['http://localhost:5173'],
+  credentials: true,
+}));
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
+
+app.get('/api/races', async (c) => {
+  const { results } = await c.env.keiba_db
+  .prepare('SELECT * FROM races')
+  .all();
+
+return c.json({
+  races: results
+});
+});
 
 export default app
