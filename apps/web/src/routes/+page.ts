6ed5b666de70bcf74a30a0b22816b2985d909268
HayatoKudo <kudoh115@gmail.com>
2025-09-28 14:25:58 +0900

    chore

--- apps/web/src/routes/+page.ts
+++ apps/web/src/routes/+page.ts
@@ -1,6 +1,9 @@
 import type { PageLoad } from './$types';
 import { PUBLIC_API_URL } from '$env/static/public';
 
+// SSRを無効化してクライアントサイドでのみ実行
+export const ssr = false;
+
 export const load: PageLoad = async () => {
   const response = await fetch(`${PUBLIC_API_URL}/api/races`);
   const data = await response.json();
